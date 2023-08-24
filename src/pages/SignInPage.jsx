c5a955617a5845265a116acb7a3e3effaaa3d120
hugo <hugoo.ribeiro03@gmail.com>
2023-08-24 12:31:08 -0300

    versao sign-in/ sign-up

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,18 +1,62 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignInPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function toDoLogin(e){
+
+    e.preventDefault();
+
+    const login = {
+      email: email,
+      password: password
+    }
+
+    const promise = axios.post(`${import.meta.env.VITE_API_URL}/sign-in`,login);
+    promise.then((res) => { 
+        //setToken(res.data.token);
+        navigate('/home');
+    });
+    promise.catch( erro => {
+        alert(erro.response)
+    }); 
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={toDoLogin}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input 
+          data-test="email"
+          placeholder="E-mail" 
+          type="email"
+          onChange={ e => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+
+        <input 
+          data-test="password"
+          placeholder="Senha" 
+          type="password" 
+          autocomplete="new-password" 
+          onChange={ e => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+
+        <button type="submit" data-test="sign-in-submit">Entrar</button>
       </form>
 
-      <Link>
+      <Link to="/cadastro">
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,17 +1,82 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUpPage() {
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(null);
+
+  const navigate = useNavigate();
+
+  function createUser(e){
+    e.preventDefault();
+
+    const newRegister = {
+      email: email,
+      name: name,
+      password: password
+    }
+
+    console.log(newRegister)
+  const promise = axios.post(`${import.meta.env.VITE_API_URL}/sign-up`,newRegister);
+  promise.then((res) => {
+      console.log(res.data)
+      //setRegister(res.data);
+      navigate('/')
+  });
+  promise.catch( erro => { 
+      alert(erro.response)});
+
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={createUser}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input 
+          data-test="name"
+          placeholder="Nome" 
+          type="text"
+          onChange={ e => setName(e.target.value)}
+          value={name}
+          required
+        />
+
+        <input 
+          data-test="email"
+          placeholder="E-mail" 
+          type="email" 
+          onChange={ e => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+
+        <input 
+          data-test="password"
+          placeholder="Senha" 
+          type="password" 
+          autocomplete="new-password" 
+          onChange={ e => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+
+        <input 
+          data-test="conf-password"
+          placeholder="Confirme a senha" 
+          type="password" 
+          autocomplete="new-password" 
+          onChange={ e => setPassword(e.target.value)}                
+          value={password}
+          required
+        />
+
+        <button type="submit" data-test="sign-up-submit">Cadastrar</button>
       </form>
 
       <Link>
